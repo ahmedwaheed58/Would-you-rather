7f7cbb2fa93750b6a0266f14e5925a7fa4250c53
ahmedwaheed58 <79012660+ahmedwaheed58@users.noreply.github.com>
2021-07-20 05:35:17 +0200

    Delete src directory

--- src/middleware/index.js
+++
@@ -1,8 +0,0 @@
-import logger from './logger'
-import thunkMiddleware from 'redux-thunk'
-import { applyMiddleware } from 'redux'
-
-export default applyMiddleware(
-    thunkMiddleware,
-    logger,
-)
